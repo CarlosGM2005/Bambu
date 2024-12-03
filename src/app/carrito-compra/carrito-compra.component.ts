f10416d3243b38305fa44c4e226d819ec46d4afa
Carlos.Garcia <carlosgarmaq2005@gmail.com>
2024-12-03 18:42:59 +0100

    carritoCompra termnado

--- src/app/carrito-compra/carrito-compra.component.ts
+++ src/app/carrito-compra/carrito-compra.component.ts
@@ -7,21 +7,21 @@ import { Component } from '@angular/core';
 })
 export class CarritoCompraComponent {
   listaProductos = [
-    { nombre: 'SURTIDO Nº1', tipo: 'surtidos', precio: '18.50€', urlImg: 'assets/images/productos/SURTIDO1.jpg', urlIcono: 'assets/images/iconos/carrito-aniadir.png' },
-    { nombre: 'SURTIDO Nº2', tipo: 'surtidos', precio: '18.50€', urlImg: 'assets/images/productos/SURTIDO2.jpg', urlIcono: 'assets/images/iconos/carrito-aniadir.png' },
-    { nombre: 'SURTIDO Nº3', tipo: 'surtidos', precio: '18.50€', urlImg: 'assets/images/productos/SURTIDO3.jpg', urlIcono: 'assets/images/iconos/carrito-aniadir.png' },
-    { nombre: 'SURTIDO Nº4', tipo: 'surtidos', precio: '22.50€', urlImg: 'assets/images/productos/SURTIDO4.jpg', urlIcono: 'assets/images/iconos/carrito-aniadir.png' },
-    { nombre: 'SURTIDO Nº5', tipo: 'surtidos', precio: '16.90€', urlImg: 'assets/images/productos/SURTIDO5.jpg', urlIcono: 'assets/images/iconos/carrito-aniadir.png' },
-    { nombre: 'SURTIDO Nº6', tipo: 'surtidos', precio: '18.50€', urlImg: 'assets/images/productos/SURTIDO6.jpg', urlIcono: 'assets/images/iconos/carrito-aniadir.png' },
-    { nombre: 'ARROZ CON SOJA', tipo: 'arrozNoodles', precio: '5.90€', urlImg: '', urlIcono: 'assets/images/iconos/carrito-aniadir.png' },
-    { nombre: 'ARROZ VEGETAL', tipo: 'arrozNoodles', precio: '5.90€', urlImg: '', urlIcono: 'assets/images/iconos/carrito-aniadir.png' },
-    { nombre: 'NOODLES VEGETAL', tipo: 'arrozNoodles', precio: '5.90€', urlImg: '', urlIcono: 'assets/images/iconos/carrito-aniadir.png' },
-    { nombre: 'RAMEN CERDO', tipo: 'arrozNoodles', precio: '8.90€', urlImg: '', urlIcono: 'assets/images/iconos/carrito-aniadir.png' },
-    { nombre: 'RAMEN YASAI VEGETAL', tipo: 'arrozNoodles', precio: '7.95€', urlImg: '', urlIcono: 'assets/images/iconos/carrito-aniadir.png' },
+    { nombre: 'SURTIDO Nº1', tipo: 'SURTIDOS', precio: '18.50€', urlImg: 'assets/images/productos/SURTIDO1.jpg', urlIcono: 'assets/images/iconos/carrito-aniadir.png' },
+    { nombre: 'SURTIDO Nº2', tipo: 'SURTIDOS', precio: '18.50€', urlImg: 'assets/images/productos/SURTIDO2.jpg', urlIcono: 'assets/images/iconos/carrito-aniadir.png' },
+    { nombre: 'SURTIDO Nº3', tipo: 'SURTIDOS', precio: '18.50€', urlImg: 'assets/images/productos/SURTIDO3.jpg', urlIcono: 'assets/images/iconos/carrito-aniadir.png' },
+    { nombre: 'SURTIDO Nº4', tipo: 'SURTIDOS', precio: '22.50€', urlImg: 'assets/images/productos/SURTIDO4.jpg', urlIcono: 'assets/images/iconos/carrito-aniadir.png' },
+    { nombre: 'SURTIDO Nº5', tipo: 'SURTIDOS', precio: '16.90€', urlImg: 'assets/images/productos/SURTIDO5.jpg', urlIcono: 'assets/images/iconos/carrito-aniadir.png' },
+    { nombre: 'SURTIDO Nº6', tipo: 'SURTIDOS', precio: '18.50€', urlImg: 'assets/images/productos/SURTIDO6.jpg', urlIcono: 'assets/images/iconos/carrito-aniadir.png' },
+    { nombre: 'ARROZ CON SOJA', tipo: 'ARROZ Y NOODLES', precio: '5.90€', urlImg: 'assets/images/productos/Arroz_con_Soja.png', urlIcono: 'assets/images/iconos/carrito-aniadir.png' },
+    { nombre: 'ARROZ VEGETAL', tipo: 'ARROZ Y NOODLES', precio: '5.90€', urlImg: 'assets/images/productos/ARROZ_VEGETAL.jpg', urlIcono: 'assets/images/iconos/carrito-aniadir.png' },
+    { nombre: 'NOODLES VEGETAL', tipo: 'ARROZ Y NOODLES', precio: '5.90€', urlImg: 'assets/images/productos/NOODLES.jpg', urlIcono: 'assets/images/iconos/carrito-aniadir.png' },
+    { nombre: 'RAMEN CERDO', tipo: 'ARROZ Y NOODLES', precio: '8.90€', urlImg: 'assets/images/productos/Ramen_Cerdo.png', urlIcono: 'assets/images/iconos/carrito-aniadir.png' },
+    { nombre: 'RAMEN YASAI VEGETAL', tipo: 'ARROZ Y NOODLES', precio: '7.95€', urlImg: 'assets/images/productos/Ramen_Yasai.png', urlIcono: 'assets/images/iconos/carrito-aniadir.png' },
   ];
 
-  productos: any[] = this.listaProductos.filter((producto) => producto.tipo === 'surtidos');
-
+  productos: any[] = this.listaProductos.filter((producto) => producto.tipo === 'SURTIDOS');
+  categoria: string = 'SURTIDOS';
 
 
   productosFiltrados(categoria: string) {
@@ -31,6 +31,7 @@ export class CarritoCompraComponent {
   filtrarPorCategoria(event: Event, categoria: string) {
     event.preventDefault();
     this.productos = this.productosFiltrados(categoria);
+    this.categoria = categoria;
   }
 
 }
