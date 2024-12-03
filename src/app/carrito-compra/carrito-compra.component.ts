9a8ce73ce8cc0ff551863aa5dc88eb3f2c05f6b9
Carlos.Garcia <carlosgarmaq2005@gmail.com>
2024-12-03 18:16:31 +0100

    cambios
    He cambiado el nombre de algunas de las fotos y he generado dinamicamente los productos usando ngfor

--- src/app/carrito-compra/carrito-compra.component.ts
+++ src/app/carrito-compra/carrito-compra.component.ts
@@ -6,48 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./carrito-compra.component.scss']
 })
 export class CarritoCompraComponent {
-  categorias = {
-    entrantes: [
-      {
-        nombre: 'California Roll',
-        foto: 'https://via.placeholder.com/150?text=California+Roll',
-        precio: 5.99,
-        icono: 'https://via.placeholder.com/50?text=Icon+Roll'
-      },
-      {
-        nombre: 'Tuna Roll',
-        foto: 'https://via.placeholder.com/150?text=Tuna+Roll',
-        precio: 6.49,
-        icono: 'https://via.placeholder.com/50?text=Icon+Roll'
-      },
-      {
-        nombre: 'Spicy Tuna Roll',
-        foto: 'https://via.placeholder.com/150?text=Spicy+Tuna+Roll',
-        precio: 6.99,
-        icono: 'https://via.placeholder.com/50?text=Icon+Roll'
-      }
-    ],
-    postres: [
-      {
-        nombre: 'Salmon Sashimi',
-        foto: 'https://via.placeholder.com/150?text=Salmon+Sashimi',
-        precio: 7.99,
-        icono: 'https://via.placeholder.com/50?text=Icon+Sashimi'
-      },
-      {
-        nombre: 'Tuna Sashimi',
-        foto: 'https://via.placeholder.com/150?text=Tuna+Sashimi',
-        precio: 8.49,
-        icono: 'https://via.placeholder.com/50?text=Icon+Sashimi'
-      },
-      {
-        nombre: 'Ebi Nigiri',
-        foto: 'https://via.placeholder.com/150?text=Ebi+Nigiri',
-        precio: 5.49,
-        icono: 'https://via.placeholder.com/50?text=Icon+Nigiri'
-      }
-    ]
-  };
+  listaProductos = [
+    { nombre: 'SURTIDO Nº1', tipo: 'surtidos', precio: '18.50€', urlImg: 'assets/images/productos/SURTIDO1.jpg', urlIcono: 'assets/images/iconos/carrito-aniadir.png' },
+    { nombre: 'SURTIDO Nº2', tipo: 'surtidos', precio: '18.50€', urlImg: 'assets/images/productos/SURTIDO2.jpg', urlIcono: 'assets/images/iconos/carrito-aniadir.png' },
+    { nombre: 'SURTIDO Nº3', tipo: 'surtidos', precio: '18.50€', urlImg: 'assets/images/productos/SURTIDO3.jpg', urlIcono: 'assets/images/iconos/carrito-aniadir.png' },
+    { nombre: 'SURTIDO Nº4', tipo: 'surtidos', precio: '22.50€', urlImg: 'assets/images/productos/SURTIDO4.jpg', urlIcono: 'assets/images/iconos/carrito-aniadir.png' },
+    { nombre: 'SURTIDO Nº5', tipo: 'surtidos', precio: '16.90€', urlImg: 'assets/images/productos/SURTIDO5.jpg', urlIcono: 'assets/images/iconos/carrito-aniadir.png' },
+    { nombre: 'SURTIDO Nº6', tipo: 'surtidos', precio: '18.50€', urlImg: 'assets/images/productos/SURTIDO6.jpg', urlIcono: 'assets/images/iconos/carrito-aniadir.png' },
+    { nombre: 'ARROZ CON SOJA', tipo: 'arrozNoodles', precio: '5.90€', urlImg: '', urlIcono: 'assets/images/iconos/carrito-aniadir.png' },
+    { nombre: 'ARROZ VEGETAL', tipo: 'arrozNoodles', precio: '5.90€', urlImg: '', urlIcono: 'assets/images/iconos/carrito-aniadir.png' },
+    { nombre: 'NOODLES VEGETAL', tipo: 'arrozNoodles', precio: '5.90€', urlImg: '', urlIcono: 'assets/images/iconos/carrito-aniadir.png' },
+    { nombre: 'RAMEN CERDO', tipo: 'arrozNoodles', precio: '8.90€', urlImg: '', urlIcono: 'assets/images/iconos/carrito-aniadir.png' },
+    { nombre: 'RAMEN YASAI VEGETAL', tipo: 'arrozNoodles', precio: '7.95€', urlImg: '', urlIcono: 'assets/images/iconos/carrito-aniadir.png' },
+  ];
 
+  productos: any[] = this.listaProductos.filter((producto) => producto.tipo === 'surtidos');
+
+
+
+  productosFiltrados(categoria: string) {
+    return this.listaProductos.filter((producto) => producto.tipo === categoria);
+  }
+
+  filtrarPorCategoria(event: Event, categoria: string) {
+    event.preventDefault();
+    this.productos = this.productosFiltrados(categoria);
+  }
 
 }
