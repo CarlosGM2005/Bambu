0f5ddd3b7e53b6f85e141bce14e32f1d52c4bdd5
CarlosGM2005 <117672170+CarlosGM2005@users.noreply.github.com>
2024-12-03 14:41:35 +0100

    avance carrito

--- src/app/carrito-compra/carrito-compra.component.ts
+++ src/app/carrito-compra/carrito-compra.component.ts
@@ -6,6 +6,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./carrito-compra.component.scss']
 })
 export class CarritoCompraComponent {
+  categorias = {
+    entrantes: [
+      {
+        nombre: 'California Roll',
+        foto: 'https://via.placeholder.com/150?text=California+Roll',
+        precio: 5.99,
+        icono: 'https://via.placeholder.com/50?text=Icon+Roll'
+      },
+      {
+        nombre: 'Tuna Roll',
+        foto: 'https://via.placeholder.com/150?text=Tuna+Roll',
+        precio: 6.49,
+        icono: 'https://via.placeholder.com/50?text=Icon+Roll'
+      },
+      {
+        nombre: 'Spicy Tuna Roll',
+        foto: 'https://via.placeholder.com/150?text=Spicy+Tuna+Roll',
+        precio: 6.99,
+        icono: 'https://via.placeholder.com/50?text=Icon+Roll'
+      }
+    ],
+    postres: [
+      {
+        nombre: 'Salmon Sashimi',
+        foto: 'https://via.placeholder.com/150?text=Salmon+Sashimi',
+        precio: 7.99,
+        icono: 'https://via.placeholder.com/50?text=Icon+Sashimi'
+      },
+      {
+        nombre: 'Tuna Sashimi',
+        foto: 'https://via.placeholder.com/150?text=Tuna+Sashimi',
+        precio: 8.49,
+        icono: 'https://via.placeholder.com/50?text=Icon+Sashimi'
+      },
+      {
+        nombre: 'Ebi Nigiri',
+        foto: 'https://via.placeholder.com/150?text=Ebi+Nigiri',
+        precio: 5.49,
+        icono: 'https://via.placeholder.com/50?text=Icon+Nigiri'
+      }
+    ]
+  };
 
-  
+
 }
