9e1206404cecec4390516bdd6303f7b6628d3120
mgarsau <mariags0910@gmail.com>
2024-12-03 18:19:28 +0100

    avanceLoginYReservar

--- src/app/reservar/reservar.component.ts
+++ src/app/reservar/reservar.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./reservar.component.scss']
 })
 export class ReservarComponent {
+  mensajeVisible: boolean = false;
 
+  finalizarReserva() {
+    this.mensajeVisible = true;
+
+    setTimeout(() => {
+      this.mensajeVisible = false; // Oculta el mensaje después de 5 segundos
+    }, 5000);
+  }
 }
