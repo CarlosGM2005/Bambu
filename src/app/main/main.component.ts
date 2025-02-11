529baae714a296349269d4674b6768a2c883fc66
CarlosGM2005 <117672170+CarlosGM2005@users.noreply.github.com>
2025-02-11 13:44:53 +0100

    actualizacion main

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit  } from '@angular/core';
+import { ConsumoApiService } from '../consumo-api.service'; // Asegúrate de importar el servicio
 
 @Component({
   selector: 'app-main',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./main.component.scss']
 })
 export class MainComponent {
+  locales: any[] = []; // Variable para almacenar los locales
 
+  constructor(private apiService: ConsumoApiService) { }
+
+  ngOnInit(): void {
+    this.apiService.obtenerLocales().subscribe(
+      response => {
+        if (response.status === 'success') {
+          this.locales = response.data; // Guardamos los locales recibidos
+        } else {
+          console.error('Error al obtener locales:', response.data);
+        }
+      },
+      error => {
+        console.error('Error en la solicitud:', error);
+      }
+    );
+  }
 }
